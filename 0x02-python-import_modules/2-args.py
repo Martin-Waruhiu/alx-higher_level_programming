037ef90a1531e2d248c64fb648a3fba9272687f9
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-01-25 00:24:21 -0800

    c

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,7 @@ if length == 1:
     print("0 arguments.")
 elif length == 2:
     print("1 argument:")
-    print("1:{}".format(sys.argv[1]))
 else:
-    print("{} arguments".format(length))
+    print("{} arguments:".format(length))
     for x in range(length):
         print("{}:{}".format(x + 1, sys.argv[x + 1]))
