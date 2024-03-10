08dcc7dd4ff8c9af9ef86f33b1b7f758e6808e86
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-03-09 23:22:56 -0800

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,6 @@
 
 def append_write(filename="", text=""):
     """apeend to  a file defintion"""
-
     with open(filename, "a", encoding="UTF-8") as f:
         num_char = f.write(text)
         return(num_char)
