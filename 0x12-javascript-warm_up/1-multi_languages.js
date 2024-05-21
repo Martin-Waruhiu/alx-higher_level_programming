4ffd36c0ebc9281fb5bf1aab90d2c8654b3f7993
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-05-21 05:11:58 -0700

    three prints

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// 3prints
+
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'Javascript is amazing';
+
+console.log(first);
+console.log(second);
+console.log(third);
