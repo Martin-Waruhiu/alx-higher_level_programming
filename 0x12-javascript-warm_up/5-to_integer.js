c8595f307ee7dd139c6b97db984e486c59b0cca7
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-05-22 02:20:55 -0700

    parseint

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2); // Get the arguments passed to the script
+const num = parseInt(args[0], 10); // Convert the first argument to an integer
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
