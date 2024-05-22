f3aaeccb4d05245a7c105a91e73bc564340df894
Martin-Waruhiu <migwiwaruhiu@gmail.com>
2024-05-22 03:42:50 -0700

    loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const x = parseInt(process.argv[2], 10);
 
-const num = parseInt(args[0], 10);
-
-if (!isNaN(num)) {
-  for (let i = 0; i < num; i++) {
-    console.log('C is cool');
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
